453bf4031af7c48cc12edad45142bcb7c0ffc85b
wernerlaude <mail@laude.dev>
2025-06-20 22:06:12 +0200

    Aufgeräumt - Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "survey_responses/new"
   get "survey_responses/create"
 
-  resources :survey_responses, only: [:new, :create, :show] do
+  resources :survey_responses, only: [ :new, :create, :show ] do
     collection do
       get :thank_you
     end
